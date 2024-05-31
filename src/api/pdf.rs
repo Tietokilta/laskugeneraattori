209fa0c7882e3f3e4ea138d1d01f6186c7d0ac4e
Luukas Pörtfors <lajp@iki.fi>
2024-05-31 11:53:26 +0300

    fix: don't unwrap() in the pdf-endpoint

--- src/api/pdf.rs
+++ src/api/pdf.rs
@@ -2,20 +2,23 @@ use axum::{
     body::Body,
     extract::Path,
     http::{header, Response},
-    response::IntoResponse,
 };
 use typst::model::Document;
 
 use crate::database::DatabaseConnection;
+use crate::error::Error;
 
-pub async fn pdf(mut conn: DatabaseConnection, Path(id): Path<i32>) -> impl IntoResponse {
-    let invoice = conn.get_invoice(id).await.unwrap();
-    let document: Document = invoice.try_into().unwrap();
+pub async fn pdf(
+    mut conn: DatabaseConnection,
+    Path(id): Path<i32>,
+) -> Result<Response<Body>, Error> {
+    let invoice = conn.get_invoice(id).await?;
+    let document: Document = invoice.try_into()?;
     let pdf = typst_pdf::pdf(&document, typst::foundations::Smart::Auto, None);
 
-    Response::builder()
+    Ok(Response::builder()
         .status(200)
         .header(header::CONTENT_TYPE, "application/pdf")
         .body(Body::from(pdf))
-        .unwrap()
+        .unwrap())
 }
